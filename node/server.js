63bcbbe68371b988da6d695512ad5ee254595986
Sharon <sharonsharon9048461583@gmail.com>
2025-08-14 14:17:07 +0530

    created a basic express server

--- node/server.js
+++ node/server.js
@@ -1,15 +1,36 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer(( req, res ) => {
+// const server = http.createServer(( req, res ) => {
 
-    if ( req.url === "/") {
+//     if ( req.url === "/") {
 
-        res.end("hellow this is your http server!");
-    } else if ( req.url === "/hai") {
-        res.end("hai this is your http server")
-    }
+//         res.end("hellow this is your http server!");
+//     } else if ( req.url === "/hai") {
+//         res.end("hai this is your http server")
+//     }
+// });
+
+// server.listen(3000, () => {
+//     console.log("server running at : http://localhost:3000")
+// });
+
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000 ;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hellow this is your express server");
 });
 
-server.listen(3000, () => {
-    console.log("server running at : http://localhost:3000")
+app.get("/hai", ( req, res ) => {
+    res.send("hai this is your express server");
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
 });
